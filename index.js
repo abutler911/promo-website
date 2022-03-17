a7c3f77e63a858c4c4b5a3e1fba488a33a9f1d44
abutler911 <abutler911@gmail.com>
2022-03-16 20:38:10 -0600

    adding form modal and footer

--- index.js
+++ index.js
@@ -25,3 +25,16 @@ tabs.forEach((tab) => {
         });
     });
 });
+
+//modal
+const moreInfoButton = document.querySelector('#moreinfo');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+moreInfoButton.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+});
